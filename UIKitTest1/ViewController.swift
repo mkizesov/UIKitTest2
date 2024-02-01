5ace822253e1b5c25b3e26b38a2349244a9a7fb5
M K <miron-k@ya.ru>
2024-02-01 20:22:23 +0300

    Add DamnRectangle, remove storyboard

--- UIKitTest1/ViewController.swift
+++ UIKitTest1/ViewController.swift
@@ -11,9 +11,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .white
+        setupView()
     }
-
-
+    
+    func setupView() {
+        let damnRectangle = DamnRectangle()
+        damnRectangle.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(damnRectangle)
+        
+        NSLayoutConstraint.activate([
+            damnRectangle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+            damnRectangle.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            damnRectangle.widthAnchor.constraint(equalToConstant: 100),
+            damnRectangle.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
 }
 
