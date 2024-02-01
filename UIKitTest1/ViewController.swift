d203ee996f59286db79ce079a955b5a2ec8b1de0
Miron Kizesov <miron@coperniq.io>
2024-02-01 17:48:53 +0300

    Initial Commit

---
+++ UIKitTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTest1
+//
+//  Created by M K on 01.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
